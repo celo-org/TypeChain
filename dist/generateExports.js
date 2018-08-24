9b4e0206343bef3cffc3173b385b895e22f5a5a0
Martin Chrzanowski <martin@celo.org>
2018-08-23 17:30:54 -0700

    Declare TruffleContract superclass

--- dist/generateExports.js
+++ dist/generateExports.js
@@ -8,11 +8,11 @@ function generateExports(buildFolder, exportFilename, exportFileExtension, ignor
         .readdirSync(buildFolder)
         .filter((filename) => filename.endsWith(exportFileExtension) && !ignoreFilenames.includes(filename));
     const contractNames = files.map(file => file.replace(exportFileExtension, ""));
-    let exportContent = "";
+    let exportContent = "import { TruffleContract } from './typechain-runtime';";
     contractNames.forEach(contractName => {
         exportContent = exportContent.concat(`import { ${contractName} } from "./${contractName}";\n`);
     });
-    exportContent = exportContent.concat(`\nexport {\n  ${contractNames.join(",\n  ")}\n};`);
+    exportContent = exportContent.concat(`\nexport {\n  ${contractNames.concat("TruffleContract").join(",\n  ")}\n};`);
     const exportFilepath = path.join(buildFolder, exportFilename);
     fs.writeFileSync(exportFilepath, prettier.format(exportContent, prettierConfig));
 }
--- runtime/typechain-runtime.d.ts
+++ runtime/typechain-runtime.d.ts
@@ -1,6 +1,14 @@
 /* tslint:disable */
 import { BigNumber } from "bignumber.js";
 
+export declare class TruffleContract {
+  static setProvider(provider: any): void
+
+  address: string
+  // TODO(Martin): precise ABI typing
+  abi: any
+}
+
 declare interface Transaction {
   tx: string;
   receipt: Receipt;
